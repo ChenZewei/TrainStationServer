8b71c850c1ca06a4e8c9314847b33dc1a18195b2
Zewei Chen <czwking1991@gmail.com>
2015-01-14 11:07:26 +0800

    version 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace TrainStationServer
 {
@@ -20,10 +23,57 @@ namespace TrainStationServer
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Server server;
+        private Socket socket, client;
+        private IPEndPoint ipEnd;
+        private Thread mainThread;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        ~MainWindow()
+        {
+            //if (mainThread.IsAlive)
+                mainThread.Abort();
+        }
+
+        private void Start_Click_1(object sender, RoutedEventArgs e)
+        {
+            ipEnd = new IPEndPoint(IPAddress.Any, 8888);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipEnd);
+            socket.Listen(20);
+            mainThread = new Thread(Listening);
+            mainThread.Start();
+            Result.AppendText("Start listening...\n");
+        }
+
+        private void Listening()
+        {
+            Thread clientThread;
+            while (true)
+            {
+                this.Dispatcher.BeginInvoke(new Action(() =>  Result.AppendText("Wait for accepting...\n")));
+                client = socket.Accept();
+                clientThread = new Thread(Proc);
+                this.Dispatcher.BeginInvoke(new Action(() => Result.AppendText("Accepted!\n")));
+            }
+        }
+
+        private void Proc()
+        {
+
+        }
+
+        private void onConnectRequest(IAsyncResult ar)
+        {
+            Socket Server = (Socket)ar.AsyncState;
+            Socket Client = Server.EndAccept(ar);
+        }
+
+        private void Analysis()
+        {
+
         }
     }
 }
